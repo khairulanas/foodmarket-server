d7e1d1f0be1dd689c63ef60ef90296d9cc4ade20
khairul anas <mafiakanan@gmail.com>
2020-12-22 13:07:24 +0700

    User controller > Login

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function login(Request $request)
+    {
+        try {
+            // validasi input
+            $request->validate([
+                'email' => 'email|required',
+                'password' => 'required'
+            ]);
+
+            // cek credential
+            $credentials = request(['email', 'password']);
+            if (!Auth::attempt($credentials)) {
+                return ResponseFormatter::error(
+                    ['message' => 'Unauthorized'],
+                    'Authentication failed',
+                    500
+                );
+            }
+
+            //jika hash password tidak sesuai maka beri error
+            $user = User::where('email', $request->email)->first();
+            if (!Hash::check($request->password, $user->password, [])) {
+                throw new \Exception('Invalid Credential');
+            }
+
+            //jika berhasil maka kirim token
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+            return ResponseFormatter::success(
+                ['access_token' => $tokenResult, 'token_type' => 'Bearer', 'user' => $user],
+                'Authenticated'
+            );
+        } catch (Exception $e) {
+            return ResponseFormatter::error(
+                ['message' => 'Something went wrong', 'error' => $e],
+                'Authentication Failed',
+                500
+            );
+        }
+    }
+}
