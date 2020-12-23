a3c895fa5f6f498229cb0ac223fefa29bcdf7874
khairul anas <mafiakanan@gmail.com>
2020-12-23 16:58:20 +0700

    route api - user

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+//route yang membutuhkan akses token (sudah login)
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', [UserController::class, 'fetch']);
+    Route::post('user', [UserController::class, 'updateProfile']);
+    Route::post('user/photo', [UserController::class, 'updatePhoto']);
+    Route::post('logout', [UserController::class, 'logout']);
 });
+
+//route yg tidak membutuhka  token (belum login)
+//route '/login' [class, 'function']
+Route::post('login', [UserController::class, 'login']);
+Route::post('register', [UserController::class, 'register']);
